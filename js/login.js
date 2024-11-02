6afae4316c271d91d3031342ba2955939608bfe9
Randy939 <randychirinos223@gmail.com>
2024-11-02 18:12:37 -0500

    cambios en la url login

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
     
     try {
-        const response = await fetch('https://tudominio.infinityfree.com/gran_appetit/app/Controllers/login.php', {
+        const response = await fetch('http://gran-appetit.000.pe/gran_appetit/app/Controllers/login.php', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
