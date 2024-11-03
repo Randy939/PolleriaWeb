6a98d6a72e7937d7917c1e7c267a19f30001f618
Randy939 <randychirinos223@gmail.com>
2024-11-03 12:30:53 -0500

    env

--- app/config/database.php
+++ app/config/database.php
@@ -1,16 +1,28 @@
 <?php
 class Database {
-    private $host = "MYSQL9001.site4now.net";
-    private $database_name = "db_aaeed7_polleri";
-    private $username = "aaeed7_polleri";
-    private $password = "TEadoros123k";
+    private $host;
+    private $database_name;
+    private $username;
+    private $password;
+    private $port;
     public $conn;
+
+    public function __construct() {
+        // Cargar variables de entorno
+        $this->host = getenv('DB_HOST');
+        $this->database_name = getenv('DB_NAME');
+        $this->username = getenv('DB_USER');
+        $this->password = getenv('DB_PASS');
+        $this->port = getenv('DB_PORT');
+    }
 
     public function getConnection() {
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->database_name . ";port=3306",
+                "mysql:host=" . $this->host . 
+                ";dbname=" . $this->database_name . 
+                ";port=" . $this->port,
                 $this->username,
                 $this->password,
                 array(PDO::ATTR_TIMEOUT => 5)
